82fc964f0b5cfd7f87014ad3b43647fccb164176
olga-kacala <olga.kacala@gmail.com>
2023-02-08 13:14:03 +0100

    Login function added

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,19 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCg4UBblEWSPz_Io8B0NeTD7-6on3rsGh8",
-    authDomain: "pandoteka-711cd.firebaseapp.com",
-    projectId: "pandoteka-711cd",
-    storageBucket: "pandoteka-711cd.appspot.com",
-    messagingSenderId: "269061830707",
-    appId: "1:269061830707:web:89ec9834aef899c02b0b46"
-  };
-
-
-  export const app = initializeApp(firebaseConfig);
-  export const db = getFirestore(app);
-  export const auth = getAuth(app);
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyCg4UBblEWSPz_Io8B0NeTD7-6on3rsGh8",
+  authDomain: "pandoteka-711cd.firebaseapp.com",
+  projectId: "pandoteka-711cd",
+  storageBucket: "pandoteka-711cd.appspot.com",
+  messagingSenderId: "269061830707",
+  appId: "1:269061830707:web:89ec9834aef899c02b0b46",
+};
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { firebaseConfig } from './firebase';
+
+
+
+  const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(firebaseApp);
+export const firebaseDb = getFirestore(firebaseApp);
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
