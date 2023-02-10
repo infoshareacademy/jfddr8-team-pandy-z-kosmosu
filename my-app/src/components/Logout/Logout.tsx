cfebf1b4e30b0074d2759e686bd7a3b71e317f1d
Łukasz Śmigiel <81709506+L-Smigiel@users.noreply.github.com>
2023-02-10 13:55:12 +0100

    created logout component

---
+++ my-app/src/components/Logout/Logout.tsx
@@ -0,0 +1,19 @@
+import { BookList } from '../BooksList/BooksList';
+import { SearchForm } from '../SearchForm/SearchForm';
+import Slider from '../Slider/Slider';
+import classes from './Home.module.css';
+
+export const Logout = (): JSX.Element => {
+	return (
+		<div>
+			<header className={classes.header}>
+				<Slider />
+				<div className={classes.content}>
+					<h1>find the book you need</h1>
+					<SearchForm />
+				</div>
+			</header>
+			<BookList />
+		</div>
+	);
+}
