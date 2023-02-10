66b303e722bc8e166296496f0cbd3f72728fdf70
Weronika Niekludow <118271719+weronika-niekludow@users.noreply.github.com>
2023-02-10 14:41:47 +0100

    bookDetails-logged-template

--- my-app/src/components/BookDetails/BookDetails.tsx
+++ my-app/src/components/BookDetails/BookDetails.tsx
@@ -1,122 +1,240 @@
-import { useState, useEffect, useContext } from 'react';
-import { useParams } from 'react-router-dom';
-import classes from './BookDetails.module.css';
-import { Loader } from '../Loader/Loader';
-import coverImg from '../../Graphics/cover_not_found.jpg';
-import pandaFull from '../../Graphics/panda-full-mark.jpg';
-import pandaHalf from '../../Graphics/panda-half-mark.jpg';
-import { AppContext } from '../../providers/AppProvider';
-import { Link } from 'react-router-dom';
+import { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import classes from "./BookDetails.module.css";
+import { Loader } from "../Loader/Loader";
+import coverImg from "../../Graphics/cover_not_found.jpg";
+import pandaFull from "../../Graphics/panda-full-mark.jpg";
+import pandaHalf from "../../Graphics/panda-half-mark.jpg";
+import { AppContext } from "../../providers/AppProvider";
+import { Link } from "react-router-dom";
+import styles from "./BookDetails.module.css";
+import userIcon from "../../Graphics/User-icon.png";
 
-const URL = 'https://openlibrary.org/works/';
+const URL = "https://openlibrary.org/works/";
 
 export const BookDetails = () => {
-	const { isLogged } = useContext(AppContext);
-	const { id } = useParams();
-	const [loading, setLoading] = useState(false);
-	const [book, setBook] = useState<any>('');
+  const { isLogged } = useContext(AppContext);
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [book, setBook] = useState<any>("");
 
-	useEffect(() => {
-		setLoading(true);
-		async function getBookDetails() {
-			try {
-				const response = await fetch(`${URL}${id}.json`);
-				const data = await response.json();
+  useEffect(() => {
+    setLoading(true);
+    async function getBookDetails() {
+      try {
+        const response = await fetch(`${URL}${id}.json`);
+        const data = await response.json();
 
-				if (data) {
-					const {
-						description,
-						title,
-						covers,
-						subject_places,
-						subject_times,
-						subjects,
-					} = data;
-					const newBook = {
-						description: description ? description : 'No description found',
-						title: title,
-						cover_img: covers
-							? `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg`
-							: coverImg,
-						subject_places: subject_places
-							? subject_places.join(', ')
-							: 'No subject places found',
-						subject_times: subject_times
-							? subject_times.join(', ')
-							: 'No subject times found',
-						subjects: subjects ? subjects.join(', ') : 'No subjects found',
-						id: id,
-					};
-					setBook(newBook);
-				} else {
-					setBook('');
-				}
-				setLoading(false);
-			} catch (error) {
-				console.log(error);
-				setLoading(false);
-			}
-		}
-		getBookDetails();
-	}, [id]);
+        if (data) {
+          const {
+            description,
+            title,
+            covers,
+            subject_places,
+            subject_times,
+            subjects,
+          } = data;
+          const newBook = {
+            description: description ? description : "No description found",
+            title: title,
+            cover_img: covers
+              ? `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg`
+              : coverImg,
+            subject_places: subject_places
+              ? subject_places.join(", ")
+              : "No subject places found",
+            subject_times: subject_times
+              ? subject_times.join(", ")
+              : "No subject times found",
+            subjects: subjects ? subjects.join(", ") : "No subjects found",
+            id: id,
+          };
+          setBook(newBook);
+        } else {
+          setBook("");
+        }
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    }
+    getBookDetails();
+  }, [id]);
 
-	if (loading) return <Loader />;
+  if (loading) return <Loader />;
 
-	return (
-		<section>
-			<div className={classes['card-book']}>
-				<div className={classes['cover-img']}>
-					<img src={book.cover_img} alt='cover img' />
-				</div>
-				<div className={classes.content}>
-					<div>
-						<span>Title: </span>
-						<span>{book.title}</span>
-					</div>
-					<div>
-						<span>Description: </span>
-						<span>{book.description}</span>
-					</div>
-					<div>
-						<span>Subject Places: </span>
-						<span>{book.subject_places}</span>
-					</div>
-					<div>
-						<span>Subject Times: </span>
-						<span>{book.subject_times}</span>
-					</div>
-					{isLogged && (
-						<div>
-							<button>Add to favorites</button>
-							<button>Go to comments...</button>
-							<div className={classes['box-panda']}>
-								<img className={classes['panda-img']} src={pandaFull} />
-								<img className={classes['panda-img']} src={pandaFull} />
-								<img className={classes['panda-img']} src={pandaFull} />
-								<img className={classes['panda-img']} src={pandaFull} />
-								<img className={classes['panda-img']} src={pandaHalf} />
-							</div>
-						</div>
-					)}
-					{!isLogged && (
-						<div>
-							<div>
-								<Link className={classes.links} to='login'>
-									Log in
-								</Link>
-								<span>to add to favorites :)</span>
-							</div>
-							<div>
-								<span>See comments or </span>
-								<Link className={classes.links} to='login'>
-									Log in
-								</Link>
-								<span>to add one!</span>
-							</div>
-						</div>
-					)}
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section className={styles.page}>
+      <section className={styles.backplusdescription}>
+        <button className={styles.backtobrowse}>Back to browse</button>
+
+        <div className={styles.bookdetalis}>
+          <div className={classes["card-book"]}>
+            <div className={classes["cover-img"]}>
+              <img
+                className={styles.pic}
+                src={book.cover_img}
+                alt="cover img"
+              />
+            </div>
+            <div className={classes.content}>
+              <div>
+                <span>
+                  <b>Title:</b>{" "}
+                </span>
+                <span>{book.title}</span>
+              </div>
+              <div>
+                <span>
+                  <b>Author:</b>{" "}
+                </span>
+                <span>{book.author}</span>
+              </div>
+              <div>
+                <span>
+                  <b>First Publish Year:</b>{" "}
+                </span>
+                <span>{book.first_publish_year}</span>
+              </div>
+              <div>
+                <span>
+                  <b>Description:</b>{" "}
+                </span>
+                <span>{book.description}</span>
+              </div>
+              <div>
+                <span>
+                  <b>Subject Places:</b>{" "}
+                </span>
+                <span>{book.subject_places}</span>
+              </div>
+              <div>
+                <span>
+                  <b>Subject Times: </b>
+                </span>
+                <span>{book.subject_times}</span>
+              </div>
+              {isLogged && (
+                <div className={styles.buttons}>
+                  <button className={styles.addtofavorites}>
+                    <b>Add to favorites</b>
+                  </button>
+                  <button className={styles.gotocomments}>
+                    Go to comments...
+                  </button>
+                  <div className={classes["box-panda"]}>
+                    <img className={classes["panda-img"]} src={pandaFull} />
+                    <img className={classes["panda-img"]} src={pandaFull} />
+                    <img className={classes["panda-img"]} src={pandaFull} />
+                    <img className={classes["panda-img"]} src={pandaFull} />
+                    <img className={classes["panda-img"]} src={pandaHalf} />
+                  </div>
+                </div>
+              )}
+              {!isLogged && (
+                <div>
+                  <div>
+                    <Link className={classes.links} to="/login">
+                      Log in
+                    </Link>
+                    <span>to add to favorites :)</span>
+                  </div>
+                  <div>
+                    <span>See comments or </span>
+                    <Link className={classes.links} to="/login">
+                      Log in
+                    </Link>
+                    <span>to add one!</span>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <p>
+        <h2>Intrested in ....? See our recommendations</h2>
+      </p>
+
+      <section className={styles.recommendations}>
+        <div className={styles.booksproposition}>
+          <img
+            className={styles.imgbook}
+            src="https://covers.openlibrary.org/b/id/10523466-L.jpg"
+            alt="cover"
+          ></img>
+          <p>
+            {" "}
+            <b>Title:</b> Harry Potter and the Order of the Phoenix
+          </p>
+          <p>
+            <b>Author:</b> J. K. Rowling
+          </p>
+          <p>
+            <b>First Publish Year:</b> 2000{" "}
+          </p>
+        </div>
+
+        <div className={styles.booksproposition}>
+          <img
+            className={styles.imgbook}
+            src="https://covers.openlibrary.org/b/id/10523466-L.jpg"
+            alt="cover"
+          ></img>
+          <p>
+            {" "}
+            <b>Title:</b> Harry Potter and the Order of the Phoenix
+          </p>
+          <p>
+            <b>Author:</b> J. K. Rowling
+          </p>
+          <p>
+            <b>First Publish Year:</b> 2000{" "}
+          </p>
+        </div>
+
+        <div className={styles.booksproposition}>
+          <img
+            className={styles.imgbook}
+            src="https://covers.openlibrary.org/b/id/10523466-L.jpg"
+            alt="cover"
+          ></img>
+          <p>
+            {" "}
+            <b>Title:</b> Harry Potter and the Order of the Phoenix
+          </p>
+          <p>
+            <b>Author:</b> J. K. Rowling
+          </p>
+          <p>
+            <b>First Publish Year:</b> 2000{" "}
+          </p>
+        </div>
+      </section>
+
+      <section className={styles.commentsarea}>
+        <div className={styles.comments}>
+          <p>
+            <h4>Comments:</h4>
+          </p>
+
+          <div className={styles.typecomment}>
+            <img className={styles.pandacomment} src={userIcon}></img>type
+            comment here <button> add</button>
+          </div>
+          <div className={styles.comment}>
+            <p>Panda wrote on 02.07.2022:</p>
+            <p> "I like book, better than bamboo c:"</p>
+          </div>
+          <div className={styles.comment}>
+            <p> DJ Pandex wrote on 02.07.2022:</p>
+            <p> "I don't like book, is very sad :(" </p>
+          </div>
+          <p> See more...</p>
+        </div>
+      </section>
+    </section>
+  );
 };
--- my-app/src/providers/AppProvider.tsx
+++ my-app/src/providers/AppProvider.tsx
@@ -1,128 +1,129 @@
-import React, { createContext, useEffect, useState } from 'react';
-import { useCallback } from 'react';
+import React, { createContext, useEffect, useState } from "react";
+import { useCallback } from "react";
 
-const URL = 'http://openlibrary.org/search.json?title=';
+const URL = "http://openlibrary.org/search.json?title=";
 
 export type Book = {
-	id: string;
-	author: string;
-	cover_id: number;
-	first_publish_year: number;
-	title: string;
+  id: string;
+  author: string;
+  cover_id: number;
+  first_publish_year: number;
+  title: string;
 };
 
 type AppContextState = {
-	searchTerm: string | null;
-	setSearchTerm: (searchTerm: string) => void;
-	books: Book[];
-	setBooks: (param: []) => void;
-	loading: boolean;
-	setLoading: (param: boolean) => void;
-	resultTitle: string | null;
-	setResultTitle: (param: string) => void;
-	username: string | null;
-	setUsername: (username: string | null) => void;
-	listSum: number;
-	setlistSum: (value: number) => void;
-	myBookList: Book[];
-	setmyBookList: (books: Book[]) => void;
-	isLogged: boolean;
-	setIsLogged: (param: boolean) => void;
+  searchTerm: string | null;
+  setSearchTerm: (searchTerm: string) => void;
+  books: Book[];
+  setBooks: (param: []) => void;
+  loading: boolean;
+  setLoading: (param: boolean) => void;
+  resultTitle: string | null;
+  setResultTitle: (param: string) => void;
+  username: string | null;
+  setUsername: (username: string | null) => void;
+  listSum: number;
+  setlistSum: (value: number) => void;
+  myBookList: Book[];
+  setmyBookList: (books: Book[]) => void;
+  isLogged: boolean;
+  setIsLogged: (param: boolean) => void;
 };
 
 type AppProviderProps = {
-	children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 export const AppContext = createContext<AppContextState>({} as AppContextState);
 
 export const AppProvider = ({ children }: AppProviderProps): JSX.Element => {
-	const [searchTerm, setSearchTerm] = useState('Harry Potter');
-	const [books, setBooks] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const [resultTitle, setResultTitle] = useState('');
+  const [searchTerm, setSearchTerm] = useState("Harry Potter");
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [resultTitle, setResultTitle] = useState("");
 
-	const [username, setUsername] = useState<string | null>('');
-	const [listSum, setlistSum] = useState<number>(0);
-	const [myBookList, setmyBookList] = useState([] as Book[]);
+  const [username, setUsername] = useState<string | null>("");
+  const [listSum, setlistSum] = useState<number>(0);
+  const [myBookList, setmyBookList] = useState([] as Book[]);
 
-	const [isLogged, setIsLogged] = useState(true);
+  const [isLogged, setIsLogged] = useState(false);
 
-	const fetchBooks = useCallback(async () => {
-		setLoading(true);
-		try {
-			const response = await fetch(`${URL}${searchTerm}`);
-			const data = await response.json();
-			const { docs } = data;
+  const fetchBooks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${URL}${searchTerm}`);
+      const data = await response.json();
+      const { docs } = data;
 
-			if (docs) {
-				const newBooks = docs
-					.slice(0, 20)
-					.map(
-						(bookSingle: {
-							key: string;
-							author_name: string;
-							cover_i: number;
-							first_publish_year: number;
-							title: string;
-						}) => {
-							const { key, author_name, cover_i, first_publish_year, title } =
-								bookSingle;
+      if (docs) {
+        const newBooks = docs
+          .slice(0, 20)
+          .map(
+            (bookSingle: {
+              key: string;
+              author_name: string;
+              cover_i: number;
+              first_publish_year: number;
+              title: string;
+            }) => {
+              const { key, author_name, cover_i, first_publish_year, title } =
+                bookSingle;
 
-							return {
-								id: key,
-								author: author_name,
-								cover_id: cover_i,
-								first_publish_year: first_publish_year,
-								title: title,
-							};
-						}
-					);
+              return {
+                id: key,
+                author: author_name,
+                cover_id: cover_i,
+                first_publish_year: first_publish_year,
+                title: title,
+              };
+            }
+          );
 
-				setBooks(newBooks);
-				console.log(newBooks);
+        setBooks(newBooks);
+        console.log(newBooks);
 
-				if (newBooks.length > 1) {
-					setResultTitle('Your Search Result:');
-				} else {
-					setResultTitle('No Search Result Found!');
-				}
-			} else {
-				setBooks([]);
-				setResultTitle('No Search Result Found!');
-			}
-			setLoading(false);
-		} catch (error) {
-			console.log(error);
-			setLoading(false);
-		}
-	}, [searchTerm]);
+        if (newBooks.length > 1) {
+          setResultTitle("Your Search Result:");
+        } else {
+          setResultTitle("No Search Result Found!");
+        }
+      } else {
+        setBooks([]);
+        setResultTitle("No Search Result Found!");
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }, [searchTerm]);
 
-	useEffect(() => {
-		fetchBooks();
-	}, [searchTerm, fetchBooks]);
+  useEffect(() => {
+    fetchBooks();
+  }, [searchTerm, fetchBooks]);
 
-	return (
-		<AppContext.Provider
-			value={{
-				username,
-				setUsername,
-				listSum,
-				setlistSum,
-				myBookList,
-				setmyBookList,
-				searchTerm,
-				setSearchTerm,
-				books,
-				setBooks,
-				loading,
-				setLoading,
-				resultTitle,
-				setResultTitle,
-				setIsLogged,
-				isLogged,
-			}}>
-			{children}
-		</AppContext.Provider>
-	);
+  return (
+    <AppContext.Provider
+      value={{
+        username,
+        setUsername,
+        listSum,
+        setlistSum,
+        myBookList,
+        setmyBookList,
+        searchTerm,
+        setSearchTerm,
+        books,
+        setBooks,
+        loading,
+        setLoading,
+        resultTitle,
+        setResultTitle,
+        setIsLogged,
+        isLogged,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
