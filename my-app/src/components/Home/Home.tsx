c80ad751b37b9186e4d4768db7356ba899dc3977
Łukasz Śmigiel <81709506+L-Smigiel@users.noreply.github.com>
2023-02-14 22:26:15 +0100

    bur repair

--- my-app/src/components/Home/Home.tsx
+++ my-app/src/components/Home/Home.tsx
@@ -4,10 +4,12 @@ import { Logout } from '../Logout/Logout';
 import { SearchForm } from '../SearchForm/SearchForm';
 import Slider from '../Slider/Slider';
 import classes from './Home.module.css';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { AppContext } from '../../providers/AppProvider';
+import { Route } from 'react-router-dom';
 
 export const Home = (): JSX.Element => {
+	
 	return (
 		<div>
 			<header className={classes.header}>
--- my-app/src/components/Logout/Logout.tsx
+++ my-app/src/components/Logout/Logout.tsx
@@ -1,21 +1,16 @@
 import { Link, useNavigate } from "react-router-dom"
 import classes from "./Logout.module.css";
 import logoutPanda from "../../Graphics/logout-panda.png";
-import { useContext, useEffect } from 'react';
-import { AppContext } from '../../providers/AppProvider';
+import { useEffect } from 'react';
 import Timer from "../Timer/Timer";
 
 export const Logout = (): JSX.Element => {
-    
-    // const {isLogged} = useContext(AppContext)
-    //   const navigate = useNavigate();
-    //   useEffect(() => {
-    //   if (!isLogged) {
-    //     setTimeout(() => {
-    //       navigate('/');
-    //     }, 5000);
-    //   }
-    // })
+      const navigate = useNavigate();
+      useEffect(() => {
+        setTimeout(() => {
+          navigate('/');
+        }, 5000);
+    })
 
   return (
     <>
