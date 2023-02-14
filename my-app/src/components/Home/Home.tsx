cf4c888a10140edee24ff115e34a4cb2bfd7a533
Łukasz Śmigiel <81709506+L-Smigiel@users.noreply.github.com>
2023-02-14 22:31:15 +0100

    merge resolve

--- my-app/src/components/Home/Home.tsx
+++ my-app/src/components/Home/Home.tsx
@@ -1,15 +1,10 @@
 import { BookDetails } from '../BookDetails/BookDetails';
 import { BookList } from '../BooksList/BooksList';
-import { Logout } from '../Logout/Logout';
 import { SearchForm } from '../SearchForm/SearchForm';
 import Slider from '../Slider/Slider';
 import classes from './Home.module.css';
-import { useContext, useEffect } from 'react';
-import { AppContext } from '../../providers/AppProvider';
-import { Route } from 'react-router-dom';
 
 export const Home = (): JSX.Element => {
-	
 	return (
 		<div>
 			<header className={classes.header}>
