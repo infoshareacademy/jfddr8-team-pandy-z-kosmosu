ff917c2461809d86894efc9e48af8c550e82208d
Łukasz Śmigiel <81709506+L-Smigiel@users.noreply.github.com>
2023-02-14 22:05:23 +0100

    styling and redirection for logout

--- my-app/src/components/Logout/Logout.tsx
+++ my-app/src/components/Logout/Logout.tsx
@@ -1,16 +1,31 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import classes from "./Logout.module.css";
 import logoutPanda from "../../Graphics/logout-panda.png";
+import { useContext, useEffect } from 'react';
+import { AppContext } from '../../providers/AppProvider';
+import Timer from "../Timer/Timer";
 
 export const Logout = (): JSX.Element => {
+    
+    // const {isLogged} = useContext(AppContext)
+    //   const navigate = useNavigate();
+    //   useEffect(() => {
+    //   if (!isLogged) {
+    //     setTimeout(() => {
+    //       navigate('/');
+    //     }, 5000);
+    //   }
+    // })
+
   return (
+    <>
     <div className={classes.logout}>
       <img src={logoutPanda} alt="" />
       <div>
         <h2>You've been logged out</h2>
-        <div>
+        <div className={classes.remember}>
           <h4>Please remember about me or</h4>
-          <button>
+          <button className={classes.login}>
             <Link to="/login">
               Log in
             </Link>
@@ -18,5 +33,7 @@ export const Logout = (): JSX.Element => {
         </div>
       </div>
     </div>
+    <p>Redirecting in <Timer/> seconds</p>
+    </>
   );
 };
---
+++ my-app/src/components/Timer/Timer.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const Timer = () => {
+  const [counter, setCounter] = useState(5);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <span>{counter}</span>
+  );
+}
+
+export default Timer;
