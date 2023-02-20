c35c8cd2de760b3307808c7318c4d5a8a1c1a662
olga-kacala <olga.kacala@gmail.com>
2023-02-20 18:17:23 +0100

    Scroll to filtered books function added

--- my-app/src/components/BookDetails/BookDetails.tsx
+++ my-app/src/components/BookDetails/BookDetails.tsx
@@ -7,7 +7,7 @@ import pandaFull from "../../Graphics/panda-full-mark.jpg";
 import pandaHalf from "../../Graphics/panda-half-mark.jpg";
 import { AppContext } from "../../providers/AppProvider";
 import { Link } from "react-router-dom";
-import { firebaseDb } from "../../index";
+import { firebaseDb } from "../../App";
 import { doc, setDoc, onSnapshot } from "firebase/firestore";
 import { Comment } from "./Comment";
 import icon from "../../Graphics/User-icon.png";
@@ -30,7 +30,6 @@ export const BookDetails = (): JSX.Element => {
   const [book, setBook] = useState<any>("");
   const [myMessagesList, setmyMessagesList] = useState([] as MyComment[]);
   const [commentValue, setCommentValue] = useState("");
-  //   const navigate = useNavigate();
 
   useEffect(() => {
     setLoading(true);
--- my-app/src/components/BooksList/BooksList.tsx
+++ my-app/src/components/BooksList/BooksList.tsx
@@ -16,7 +16,6 @@ export const BookList = () => {
 				: coverImg,
 		};
 	});
-
 	if (loading) return <Loader />;
 
 	return (
