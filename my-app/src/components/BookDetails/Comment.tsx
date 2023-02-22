1270e8562b8e0f5e0d97fd29606c668b9175d886
Weronika Niekludow <118271719+weronika-niekludow@users.noreply.github.com>
2023-02-22 11:32:26 +0100

    style

--- my-app/src/components/BookDetails/Comment.tsx
+++ my-app/src/components/BookDetails/Comment.tsx
@@ -1,30 +1,30 @@
-import { AppContext } from '../../providers/AppProvider';
-import { useContext } from 'react';
-import { MyComment } from './BookDetails';
-import classes from './Comment.module.css';
+import { AppContext } from "../../providers/AppProvider";
+import { useContext } from "react";
+import { MyComment } from "./BookDetails";
+import classes from "./Comment.module.css";
 
 export type MyCommentProps = {
-	item: MyComment;
-	removeComment: (commId: number) => void;
+  item: MyComment;
+  removeComment: (commId: number) => void;
 };
 
 export const Comment = ({
-	item,
-	removeComment,
+  item,
+  removeComment,
 }: MyCommentProps): JSX.Element => {
-	const { username } = useContext(AppContext);
+  const { username } = useContext(AppContext);
 
-	return (
-		<div className={classes['single-comment']}>
-			<p>
-				Created at:
-				{new Intl.DateTimeFormat('pl-PL').format(new Date(Date.now()))}
-			</p>
-			<p>You are commenting as: {item.user}</p>
-			<p>Comment:{item.message}</p>
-			{username === item.user && (
-				<button onClick={() => removeComment(item.id)}>Remove</button>
-			)}
-		</div>
-	);
+  return (
+    <div className={classes["single-comment"]}>
+      <div className={classes.whoCommentStyle}>
+        Created at:
+        {new Intl.DateTimeFormat("pl-PL").format(new Date(Date.now()))}
+      </div>
+      <div>You are commenting as: {item.user}</div>
+      <div>Comment:{item.message}</div>
+      {username === item.user && (
+        <button onClick={() => removeComment(item.id)}>Remove</button>
+      )}
+    </div>
+  );
 };
