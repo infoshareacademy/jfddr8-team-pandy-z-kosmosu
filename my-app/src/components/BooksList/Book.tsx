b7332d12ace73262a06606df449ddbec363e0165
Weronika Niekludow <118271719+weronika-niekludow@users.noreply.github.com>
2023-02-22 11:12:09 +0100

    card-style

--- my-app/src/components/BooksList/Book.tsx
+++ my-app/src/components/BooksList/Book.tsx
@@ -1,47 +1,55 @@
-import { Link } from 'react-router-dom';
-import classes from './Book.module.css';
-import { useContext} from 'react';
-import { AppContext } from '../../providers/AppProvider';
+import { Link } from "react-router-dom";
+import classes from "./Book.module.css";
+import { useContext } from "react";
+import { AppContext } from "../../providers/AppProvider";
 
 export const Book = (book: any) => {
-	const { addToFav, isLogged, myBookList } = useContext(AppContext);
+  const { addToFav, isLogged, myBookList } = useContext(AppContext);
 
-	return (
-		<div className={classes.book}>
-			<div>
-				<img className={classes.cover} src={book.cover_img} alt='cover' />
-			</div>
-			<div>
-				<div>
-					<Link to={`/book/${book.id}`} {...book}>
-						<span>Title: </span>
-						<span className={classes.overflowEllipsis}>{book.title}</span>
-					</Link>
-				</div>
+  return (
+    <div className={classes.book}>
+      <div>
+        <img className={classes.cover} src={book.cover_img} alt="cover" />
+      </div>
+      <div>
+        <div>
+          <Link to={`/book/${book.id}`} {...book}>
+            <span>
+              <b>Title: </b>
+            </span>
+            <span className={classes.overflowEllipsis}>{book.title}</span>
+          </Link>
+        </div>
 
-				<div>
-					<span>Author: </span>
-					<span className={classes.overflowEllipsis}>{book.author}</span>
-				</div>
+        <div>
+          <span>
+            <b>Author: </b>
+          </span>
 
-				<div>
-					<span>First Publish Year: </span>
-					<span>{book.first_publish_year}</span>
-				</div>
-				{isLogged && (
-					<button
-						disabled={myBookList.some(
-							(singleBook) => singleBook.id === book.id
-						)}
-						onClick={() =>
-							addToFav({
-								title: book.title,
-								cover_img: book.cover_img,
-								id: book.id,
-							})
-						}></button>
-				)}
-			</div>
-		</div>
-	);
+          <span className={classes.overflowEllipsis}>{book.author}</span>
+        </div>
+
+        <div>
+          <span>
+            <b>First Publish Year:</b>{" "}
+          </span>
+          <span>{book.first_publish_year}</span>
+        </div>
+        {isLogged && (
+          <button
+            disabled={myBookList.some(
+              (singleBook) => singleBook.id === book.id
+            )}
+            onClick={() =>
+              addToFav({
+                title: book.title,
+                cover_img: book.cover_img,
+                id: book.id,
+              })
+            }
+          ></button>
+        )}
+      </div>
+    </div>
+  );
 };
