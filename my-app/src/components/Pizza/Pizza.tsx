bd581aa86c64264e852f4f3c87babcc728f4edb4
Lukasz Smigiel <lukasz.smigiel@gmail.com>
2023-03-06 17:09:28 +0100

    hide redux element

--- my-app/src/components/Pizza/Pizza.tsx
+++ my-app/src/components/Pizza/Pizza.tsx
@@ -1,18 +1,19 @@
 import { addTopping } from "./pizzaSlice"
 import { useDispatch, useSelector } from "react-redux";
+import classes from './Pizza.modules.css' 
 
 export const Pizza = (): JSX.Element => {
     const pizza: any = useSelector<any>(state => state.pizza);
     const dispatch = useDispatch();
 
     return (
-        <>
+        <div className={classes.pizza}>
         <p>Pizza</p>
 {pizza.toppings.map((topping: any) => (
   <div>{topping}</div>
 ))}
 <button onClick={() => dispatch(addTopping('pepperoni'))}>Add Pepperroni</button>
-</> 
+</div> 
     )
 
 }
