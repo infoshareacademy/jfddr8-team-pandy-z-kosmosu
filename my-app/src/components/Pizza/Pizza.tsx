605015843370e9bf98599273b13ae1ad7a9b4980
Lukasz Smigiel <lukasz.smigiel@gmail.com>
2023-03-06 17:12:38 +0100

    repair css

--- my-app/src/components/Pizza/Pizza.tsx
+++ my-app/src/components/Pizza/Pizza.tsx
@@ -1,13 +1,12 @@
 import { addTopping } from "./pizzaSlice"
 import { useDispatch, useSelector } from "react-redux";
-import classes from './Pizza.modules.css' 
 
 export const Pizza = (): JSX.Element => {
     const pizza: any = useSelector<any>(state => state.pizza);
     const dispatch = useDispatch();
 
     return (
-        <div className={classes.pizza}>
+        <div className="pizza">
         <p>Pizza</p>
 {pizza.toppings.map((topping: any) => (
   <div>{topping}</div>
