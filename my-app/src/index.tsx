bca8657f3bbac61bcbb916bfca186bd211203256
Lukasz Smigiel <lukasz.smigiel@gmail.com>
2023-03-06 14:43:58 +0100

    add index

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-
 import { AppProvider } from './providers/AppProvider';
 import { Provider } from 'react-redux';
 import { store } from './store';
